1ce4c30ada9b0418cd9faed6ff6a75e37d2a4dd9
Nanda-Ninou <elasmainada@gmail.com>
2024-06-23 12:59:14 +0100

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,12 @@
-# Écrivez votre code ici !
+nom='nada'
+age=19
+hight=1.68
+student=True
+print(nom='nada')
+print(age=19)
+print(hight=1.68)
+print(student=True)
+type(nom)
+type(age)
+type(hight)
+type(student)
